0a7b6289d98539f14900923da8ec913d7d7767df
Tivotal <121335284+Tivotal@users.noreply.github.com>
2024-04-19 07:54:14 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+/* Created by Tivotal */
+
+let btn = document.querySelector(".btn");
+let input = document.querySelector("input");
+
+btn.addEventListener("click", () => {
+  JsBarcode(".code", input.value, {
+    format: "code128",
+    displayValue: true,
+    fontSize: 24,
+    lineColor: "#000",
+  });
+
+  input.value = "";
+});
